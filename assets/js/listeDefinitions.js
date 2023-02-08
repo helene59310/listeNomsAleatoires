275413eaab2a1fbaa8df3c626a155aecc3c3244c
Hélène Vasseur <heloise.59@hotmail.fr>
2023-02-08 16:39:24 +0100

    maj css

--- assets/js/listeDefinitions.js
+++ assets/js/listeDefinitions.js
@@ -1,6 +1,6 @@
 function listeDefinitions(){
 var chaine="Une bobine de fil peut avoir une multitude de couleurs.| \
-Une machine à coudre n'est pas une surjeteuse.| \
-";
+Une machine à coudre n'est pas une surjeteuse.| \ Aiguilles : Pièce majeure de la machine à coudre, il existe plusieurs types d’aiguille en fonction du tissu qui doit être cousu. Pour les différencier, des chiffres allant de 60 à 110, correspondants à la taille en micron de l’aiguille. Donc, plus le chiffre est gros, plus l’aiguille l’est ! Mais ne vous fiez pas à ces grandes valeurs, c’est très difficile de les différencier à l’oeil nu! Gardez en tête que plus votre tissu est fin, plus votre aiguille doit l’être également. Et faites en sorte d’en avoir quelques unes de rechange dans votre boite à couture, car on casse une aiguille plus rapidement qu’on ne croit ! Les aiguilles sont standardisées, si bien que n'importe quelle marque fonctionnera avec votre machine.|"
+;
 return chaine;
 }
